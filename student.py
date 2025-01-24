d24aa97bbbcec710b6b0e93e3f718626a015df2b
Kundan-99 <kundanmishra2002@gmail.com>
2025-01-24 19:15:13 +0530

    Added student management module with GUI for managing student details

---
+++ student.py
@@ -0,0 +1,149 @@
+from tkinter import*
+from tkinter import ttk
+from PIL import Image,ImageTk
+from tkinter import messagebox
+import mysql.connector
+import cv2
+
+class Student:
+    def __init__(self,root):
+        self.root=root
+        self.root.geometry("1530x790+0+0")
+        self.root.title("face Recognition System")
+
+        #=========variables==============
+        self.var_dep = StringVar()
+        self.var_course = StringVar()
+        self.var_year = StringVar()
+        self.var_semester = StringVar()
+        self.var_std_id = StringVar()
+        self.var_std_name = StringVar()
+        self.var_div = StringVar()
+        self.var_reg = StringVar()
+        self.var_email = StringVar()
+        self.var_phone = StringVar()
+
+        # First Image
+        img=Image.open(r"E:\FACE @\college_images\face-recognition.png")
+        img=img.resize((500,130),Image.LANCZOS)
+        self.photoimg=ImageTk.PhotoImage(img)
+        
+        f_lbl=Label(self.root,image=self.photoimg)
+        f_lbl.place(x=0,y=0,width=400,height=110)
+
+
+        #second Image
+        img1=Image.open(r"E:\FACE @\college_images\smart-attendance.jpg")
+        img1=img1.resize((500,130),Image.LANCZOS)
+        self.photoimg1=ImageTk.PhotoImage(img1)
+        
+        f_lbl=Label(self.root,image=self.photoimg1)
+        f_lbl.place(x=400,y=0,width=500,height=110)
+        
+        #third Image
+        img2=Image.open(r"E:\FACE @\college_images\cllg.jpg")
+        img2=img2.resize((500,130),Image.LANCZOS)
+        self.photoimg2=ImageTk.PhotoImage(img2)
+        
+        f_lbl=Label(self.root,image=self.photoimg2)
+        f_lbl.place(x=900,y=0,width=500,height=110)
+
+        #bg image
+        img3=Image.open(r"E:\FACE @\college_images\bg1.jpg")
+        img3=img3.resize((1530,710),Image.LANCZOS)
+        self.photoimg3=ImageTk.PhotoImage(img3)
+        
+        bg_img=Label(self.root,image=self.photoimg3)
+        bg_img.place(x=0,y=110,width=1530,height=710)
+        
+        title_lbl=Label(bg_img,text="STUDENT MANAGEMENT SYSTEM",font=("times new roman",25,"bold"),bg="white",fg="darkgreen")
+        title_lbl.place(x=0,y=0,width=1330,height=35)
+        
+        main_frame=Frame(bg_img,bd=2,bg="white")
+        main_frame.place(x=15,y=40,width=1250,height=492)
+
+        #left label frame
+        Left_frame=LabelFrame(main_frame,bd=2,bg="white",relief=RIDGE,text="Student Details",font=("times new roman",12,"bold"))
+        Left_frame.place(x=5,y=0,width=600,height=482)
+
+        img_left=Image.open(r"E:\FACE @\college_images\Att.jpeg")
+        img_left=img_left.resize((380,70),Image.LANCZOS)
+        self.photoimg_left=ImageTk.PhotoImage(img_left)
+        
+        f_lbl=Label(Left_frame,image=self.photoimg_left)
+        f_lbl.place(x=90,y=0,width=380,height=70)
+
+        # current course information
+        current_course_frame=LabelFrame(Left_frame,bd=2,bg="white",relief=RIDGE,text="Current course information",font=("times new roman",12,"bold"))
+        current_course_frame.place(x=5,y=75,width=580,height=120)
+
+        #department
+        dep_label=Label(current_course_frame,text="Department",font=("times new roman",12,"bold"),bg="white")
+        dep_label.grid(row=0,column=0,padx=10,sticky=W)
+
+        dep_combo=ttk.Combobox(current_course_frame,textvariable=self.var_dep,font=("times new roman",12,"bold"),state="readonly")
+        dep_combo["values"]=("Select Department","Computer","IT","Civil","Mechanical")
+        dep_combo.current(0)
+        dep_combo.grid(row=0,column=1,padx=2,pady=10)
+
+        #course
+        course_label=Label(current_course_frame,text="  Course",font=("times new roman",12,"bold"),bg="white")
+        course_label.grid(row=0,column=2,padx=2,pady=10,sticky=W)
+        
+        course_combo=ttk.Combobox(current_course_frame,textvariable=self.var_course,font=("times new roman",12,"bold"),state="readonly")
+        course_combo["values"]=("Select Course","BTECH","MTECH","BCA","MBA")
+        course_combo.current(0)
+        course_combo.grid(row=0,column=3,padx=2,pady=10,sticky=W)
+        
+        #Year
+        year_label=Label(current_course_frame,text="  Year",font=("times new roman",12,"bold"),bg="white")
+        year_label.grid(row=1,column=0,padx=2,pady=10,sticky=W)
+        
+        year_combo=ttk.Combobox(current_course_frame,textvariable=self.var_year,font=("times new roman",12,"bold"),state="readonly")
+        year_combo["values"]=("Select Year","2022-23","2023-24","2024-25","2025-26")
+        year_combo.current(0)
+        year_combo.grid(row=1,column=1,padx=2,pady=10,sticky=W)
+
+        #Semester
+        semester_label=Label(current_course_frame,text="Semester",font=("times new roman",12,"bold"),bg="white")
+        semester_label.grid(row=1,column=2,padx=10,sticky=W)
+        
+        semester_combo=ttk.Combobox(current_course_frame,textvariable=self.var_semester,font=("times new roman",12,"bold"),state="readonly")
+        semester_combo["values"]=("Select Semester","Semester-1","Semester-2")
+        semester_combo.current(0)
+        semester_combo.grid(row=1,column=3,padx=2,pady=10,sticky=W)
+
+
+        # Class Student information
+        class_Student_frame=LabelFrame(Left_frame,bd=2,bg="white",relief=RIDGE,text="Class Student information",font=("times new roman",12,"bold"))
+        class_Student_frame.place(x=5,y=200,width=580,height=255)
+
+        # Student ID
+        studentId_label=Label(class_Student_frame,text="StudentID",font=("times new roman",13,"bold"),bg="white")
+        studentId_label.grid(row=0,column=0,padx=10,sticky=W)
+
+        studentID_entry=ttk.Entry(class_Student_frame,textvariable=self.var_std_id,width=15,font=("times new roman",13,"bold"))
+        studentID_entry.grid(row=0,column=1,padx=10,sticky=W)
+
+         #student name
+        studentName_label=Label(class_Student_frame,text="Student Name:",font=("times new roman",12,"bold"),bg="white")
+        studentName_label.grid(row=0,column=2,padx=10,pady=5,sticky=W)
+            
+        studentName_entry=ttk.Entry(class_Student_frame,textvariable=self.var_std_name,width=15,font=("times new roman",12,"bold"))
+        studentName_entry.grid(row=0,column=3,padx=10,pady=5,sticky=W)
+
+        #class division
+        class_div_label=Label(class_Student_frame,text="Class Division:",font=("times new roman",12,"bold"),bg="white")
+        class_div_label.grid(row=1,column=0,padx=10,pady=10,sticky=W)
+            
+        class_div_entry=ttk.Entry(class_Student_frame,textvariable=self.var_div,width=17,font=("times new roman",12,"bold"))
+        class_div_entry.grid(row=1,column=1,padx=10,pady=5,sticky=W)
+
+        #Roll No
+        roll_no_label=Label(class_Student_frame,text="Reg No:",font=("times new roman",12,"bold"),bg="white")
+        roll_no_label.grid(row=1,column=2,padx=10,pady=5,sticky=W)
+            
+        roll_no_entry=ttk.Entry(class_Student_frame,textvariable=self.var_reg,width=15,font=("times new roman",12,"bold"))
+        roll_no_entry.grid(row=1,column=3,padx=10,pady=5,sticky=W)
+        
+
